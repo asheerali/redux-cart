d3c359c3aaa0c16f3eef131e4caf5a364f3efff9
asheerali <asheerali1997@gmail.com>
2024-09-24 19:06:04 +0200

    eslint added

---
+++ src/store/cart-slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    // totalAmount: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+    },
+    removeItemFromCart() {},
+  },
+});
